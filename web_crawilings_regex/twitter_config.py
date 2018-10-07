2b08c6dd806cf8655bff36ece3dd4278bf9aa93b
Leon Kalderon <lkalderon2@gmail.com>
2018-10-07 21:51:22 +0300

    Add files via upload

---
+++ web_crawilings_regex/twitter_config.py
@@ -0,0 +1,9 @@
+config = {
+ 'consumer_key': '2YLYdQQBzw1vZN8MskUHVqokF',
+ 'consumer_secret':
+'KyYwkeYdSD9c3hXHWQs5kCkvE0mygPCZD2sDl01fpv7z4C8Sf5',
+ 'access_token':
+'334241066-pH6bHQDlSJaixVUEtgCu9aFBAlEHJldzPEDZBDpw',
+ 'access_token_secret':
+'gmBkElFmzhj2MAZIil2G4B4QttYJG9fwxgsuc81GbE6UC'
+}
